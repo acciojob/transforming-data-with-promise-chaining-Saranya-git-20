c11d6c421eb60e42d0feb8fd9d3b6724bc6e3b69
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-30 19:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function delay(ms,res){
+	return new Promise((resolve)=>{
+		setTimeout(()=>resolve(res),ms)
+	})
+}
+
+async function startProcess(){
+	 const numInput = document.getElementById("ip").value;
+	const resultDisplay = document.getElementById("output");
+
+	let num = parseFloat(numInput)
+	let result = await delay(2000, num);
+	resultDisplay.textContent = `Result: ${result}`;
+
+	let result = await delay(2000, num*2);
+	resultDisplay.textContent = `Result: ${result}`;
+
+	let result = await delay(1000, num-3);
+	resultDisplay.textContent = `Result: ${result}`;
+
+	let result = await delay(1000, num/2);
+	resultDisplay.textContent = `Result: ${result}`;
+
+	let result = await delay(1000, num+10);
+	resultDisplay.textContent = `Result: ${result}`;
+}
+document.getElementById("btn").addEventListener("click", startProcess);
